e0a002f2424bf2109a2914af6da915d4c163a75e
Suraj Van Verma <surajvanv@gmail.com>
2026-09-01 19:34:43 -0400

    Remove skhd depends_on to avoid tap conflict

--- Formula/kursor.rb
+++ Formula/kursor.rb
@@ -5,7 +5,6 @@ class Kursor < Formula
   sha256 "9128c50067081b6df3d20f6f4528daf52d1a14a7063ec0b39be9a26b36653a10"
   license "MIT"
 
-  depends_on "koekeishiya/formulae/skhd"
   depends_on :macos
 
   def install
@@ -16,6 +15,9 @@ class Kursor < Formula
 
   def caveats
     <<~EOS
+      kursor requires skhd for global hotkey support:
+        brew install koekeishiya/formulae/skhd
+
       Add your Groq API key (free at https://console.groq.com):
         security add-generic-password -U -s "com.kursor.app" -a "api_key" -w "YOUR_KEY"
 
